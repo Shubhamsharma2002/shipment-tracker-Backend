54ad3bdbfa800d375f03470119f40fa1fe92fcaa
shubham sharma <shubhamtech004@gmail.com>
2025-04-21 02:56:21 +0530

    routes setup

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,18 @@ import Statusrouter from './Routes/Status';
 
 const app = express();
 const PORT =  process.env.PORT || 8000;
-app.use(cors());
+app.use(cors({
+  origin: "*", // Or specify allowed origins
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization"
+}));
 app.use(express.json());
 
 app.get("/", (_req, res) => {
   res.send("Shipment Tracker API is running!");
 });
 // shipment routes
-app.use("/api/v1",Shipmentrouter);
+app.use("/api/v1/create",Shipmentrouter);
 // shipment status routes
 app.use('/api/v1', Statusrouter);
 
